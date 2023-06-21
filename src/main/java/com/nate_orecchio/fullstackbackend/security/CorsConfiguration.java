093e47adf0e28aa273858550b9c6ad98848e2f1e
NathanOrecchio <86748498+NathanOrecchio@users.noreply.github.com>
2023-06-21 16:05:10 -0400

    enable cors

--- src/main/java/com/nate_orecchio/fullstackbackend/security/CorsConfiguration.java
+++ src/main/java/com/nate_orecchio/fullstackbackend/security/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.nate_orecchio.fullstackbackend;
+package com.nate_orecchio.fullstackbackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
